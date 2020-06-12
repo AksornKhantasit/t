09da4fa60a2d28ee55c01627927c400d5b77754e
AKSORN KHANTASIT <aksorn.khantasit@scb.co.th>
2020-06-12 15:58:34 +0700

    Initial Commit

---
+++ testupcode/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  testupcode
+//
+//  Created by AKSORN KHANTASIT on 12/6/2563 BE.
+//  Copyright © 2563 AKSORN KHANTASIT. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
